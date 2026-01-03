0d9c3dba53c041aafd3b724c30ddf59103e6a404
Prasannakoduri <120080385+Prasannakodui@users.noreply.github.com>
2026-01-03 12:38:27 +0530

    updated code

---
+++ includes/footer.php
@@ -0,0 +1,142 @@
+
+        <section class="appointment-area-wrapper section-padding" style="background: url('assets/img/Footer up image.webp');">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="appointment-wrap">
+                        <div class="appointment-content">
+                          
+                                       <p class="appointment-text"> "MCI TOWERS " 25-12-31, KOTAVEEDHI,VISAKHAPATNAM 530001.<br>
+                                       <a href="mailto:bd@mcigroup.co
+">bd@mcigroup.co
+</a>
+                                       </p>
+                            <div class="about-btn">
+                                <a href="contact.html" class="button-two">Get In Touch <span class="box-icon-1"> <i class="fas fa-arrow-right"></i></span></a>
+                            </div>
+                        </div>
+                        <div class="appointment-contact">
+                            <h1><a href="tel:+91 9100069002">+91 9100069001 / 002</a></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ============================= Start Footer Area Section =============================================== -->
+    <section class="footer-bg-img" style="background: url('assets/img/footer-shape-1.png');">
+    
+
+        <div class="footer-widget-area section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-12">
+                        <div class="footer-widget widget1">
+                            <div class="footer-logo">
+                                <a href="home-two.html"><img src="assets/img/footer-logo.png" alt=""></a>
+                            </div>
+                            <!-- <p class="text">Sapien luctus lesuada sentus pharetra nisi quisuea aenean eros mus magnis
+                                arcu vehicula nascetur feugiat.</p> -->
+                            <div class="footer-social-link">
+                                <ul>
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <!-- <li><a href="#"><i class="fab fa-behance"></i></a></li> -->
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-md-6 col-12">
+                        <div class="footer-widget widget2">
+                            <div class="footer-title">
+                                <h3 class="title">Useful Links</h3>
+                            </div>
+                            <ul>
+                                <li><a href="#"><i class="fas fa-angle-double-right"></i> Home</a></li>
+                                <li><a href="#"><i class="fas fa-angle-double-right"></i> Aboutus</a></li>
+                                <li><a href="#"><i class="fas fa-angle-double-right"></i> Products</a></li>
+                                <li><a href="#"><i class="fas fa-angle-double-right"></i> Solutions</a></li>
+                                <li><a href="#"><i class="fas fa-angle-double-right"></i> Contact</a></li>
+                            </ul>
+                          
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-md-6 col-12">
+                        <div class="footer-widget widget3">
+                            <div class="footer-title">
+                                <h3 class="title">Defence Solutions </h3>
+                            </div>
+                            <ul>
+                                <li><a href="#"><i class="fas fa-angle-double-right"></i>  Brahm Kavach™ </a></li>
+                                <li><a href="#"><i class="fas fa-angle-double-right"></i>  ASB (Autonomous Surveillance Boats) </a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-12">
+                        <div class="footer-widget widget4">
+                            <div class="footer-title">
+                                <h3 class="title">Address</h3>
+                            </div>
+                            <!-- <p class="text">Subscribe to our newsletter to get the latest updates.</p>
+                            <form class="newsletter" action="#" method="post">
+                                <input type="email" placeholder="Enter your email address">
+                                <button class="button-one border-0" type="submit">Subscribe now</button>
+                            </form> --> 
+
+
+                            <ul class="adres_out">
+<li style="text-align: left"><a href="#">25-12-31, Kota Veedhi, Visakhapatnam,<br> India</a></li>
+<li style="text-align: left"><a href="tel:9100069001">+91 9100069001 ,<br> +91 9100069002</a></li>
+<li style="text-align: left"><a href="mailto:bd@mcigroup.co">bd@mcigroup.co</a></li>
+<li style="text-align: left"><a href="www.mcigroup.co">www.mcigroup.co</a></li>
+</ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyuright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="footer-copyright text-center">
+                            <p class="mb-0">© 2025 MCI Defence Technologies. Design & Developed by <a href="bestdesigns.in">bestdesigns.in</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Start Back To Top  -->
+    <!-- <div class="to-top" id="back-top">
+        <div class="back-top-icon">
+            <i class="fas fa-angle-up"></i>
+        </div>
+        <div class="hover-icon">
+            <i class="fas fa-angle-up"></i>
+        </div>
+    </div> -->
+
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- slick-slider  -->
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <!-- megnific popup  -->
+    <script src="assets/js/jquery-magnific-popup-min.js"></script>
+    <!-- bootstrap  -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <!-- aos  -->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <!-- main-js  -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MCI Defence</title>
+
+    <!-- google fonts  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&amp;family=Manrope:wght@200..800&amp;family=Mulish:ital,wght@0,200..1000;1,200..1000&amp;family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&amp;display=swap"
+        rel="stylesheet">
+    <!-- All CSS -->
+    <!-- bootstrap  -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- slick-slider  -->
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <!-- fontawesome  -->
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <!-- megnific popup  -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <!-- aos  -->
+    <link rel="stylesheet" href="assets/css/aos.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!-- custom css  -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <!--============================ Preloader ======================================== -->
+    <div class="preloader">
+        <div class="loader-img">
+            <img src="assets/img/footer-logo.png" alt="">
+        </div>
+        <div class="loader"></div>
+    </div>
+    <!--========== Preloader ======================== -->
+
+    <!-- start off-canvas mneu  -->
+    <div class="canvas-wrapper">
+        <nav class="side-slide">
+            <div class="off-cavas-menu-overly side-box-bar"></div>
+            <div class="offcanvas-wrap">
+                <div class="off-canvas-logo">
+                    <img src="assets/img/footer-logo.png" alt="">
+                </div>
+            </div>
+            <div class="off-canvas-content">
+                <!-- <h4>Abou Us</h4>
+                <div class="off-canvas-desc"> The argument in favor of using filler text goes something like this: If
+                    you use real content in the Consulting Process, anytime you reach a review</div> -->
+                <div class="off-canvas-contact-info">
+                    <h4 class="info-title"> Contact Info </h4>
+                    <ul class="info-list">
+                        <li><i class="fas fa-map-marker-alt"></i> 25-12-31, Kota Veedhi, Visakhapatnam</li>
+                        <li> <i aria-hidden="true" class="far fa-envelope"></i>bd@mcigroup.co , mcidefence@yahoo.com </li>
+                        <li> <i aria-hidden="true" class="fa fa-phone"></i>	+91 9100069001 / 002 </li>
+                    </ul>
+                </div>
+                <div class="off-canvas-social-links">
+                    <ul>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="close-icon">
+                <h3 class="nav01 side-box-bar">x</h3>
+            </div>
+        </nav>
+    </div>
+    <!-- end off canvas menu  -->
+
+    <!-- ===========================Mobile menu ================================== -->
+    <div class="vektor-menu-wrapper">
+        <div class="vektor-menu-area">
+            <button class="vektor-menu-toggle"><i class="fas fa-times"></i></button>
+            <div class="mobile-logo">
+                <a href="home-two.php"><img src="assets/img/footer-logo.png" alt="" title="vektor-logo"></a>
+            </div>
+            <div class="vektor-mobile-menu">
+                <ul>
+                    <li>
+                        <a href="#">Home
+                            <span class="vektor-mean-expand"></span>
+                        </a>
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="about.php">About</a></li>
+                
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- ===========================Mobile menu ================================== -->
+
+    <!-- Start Header Area  -->
+    <header class="header-area home-one-page">
+        <div class="stickey-wrapper">
+            <div class="main-menu">
+                <div class="container-fluid">
+                    <div class="row small-device">
+                        <div class="col-auto col-xxl-3 col-xl-3 col-md-3 all-padding-0">
+                            <div class="home-menu-one">
+                                <div class="menu-icon-wrapper">
+                                    <div class="canva-circle side-box-bar">
+                                        <span class="canva-line line-1"></span>
+                                        <span class="canva-line line-2"></span>
+                                        <span class="canva-line line-3"></span>
+                                        <span class="canva-line line-4"></span>
+                                    </div>
+                                </div>
+                                <div class="home-one-logo">
+                                    <a href="/" class="barnds"><img src="assets/img/logo-main.png
+"
+                                            alt=""></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto col-xxl-7 col-xl-9 col-md-9 all-padding-0">
+                            <div class="home-one-menubar-area">
+                                <!--<div class="header_top">-->
+                                <!--    <div class="header_top_left">-->
+                                <!--        <ul>-->
+                                <!--            <li><i class="fas fa-map-marker-alt"></i> 25-12-31, Kota Veedhi, Visakhapatnam.</li>-->
+                                <!--            <li><i class="fas fa-envelope" aria-hidden="true"></i> Email us: <a-->
+                                <!--                    href="#">bd@mcigroup.co</a>,-->
+                                <!--                    <a-->
+                                <!--                    href="#">mcidefence@yahoo.com</a>-->
+                                <!--                    </li>-->
+                                <!--        </ul>-->
+                                <!--    </div>-->
+                             
+                                <!--</div>-->
+                                <div class="main-menu">
+                                    <ul>
+                                        <li>
+                                            <a href="index.php">Home</a>
+                                     
+                                        </li>
+                                        <li>
+                                            <a href="about.php">Aboutus</a>
+                                           
+                                        </li>
+                                        <li>
+                                            <a href="products.php">Products</a>
+                                        
+                                        </li>
+                                        <li>
+                                            <a href="solutions.php">Solutions</a>
+                                        
+                                        </li>
+                                        <li>
+                                            <a href="contactus.php">Contact</a>
+                                        
+                                        </li>   
+                                    </ul>
+                                    <div class="about-btn">
+                                        <a href="#" class="button-two">Get In Touch <span class="box-icon"><i
+                                                    class="fas fa-arrow-right"></i></span></a>
+                                    </div>
+                                </div>
+                                <button type="button" class="vektor-menu-toggle menuShow d-inline-block d-lg-none"><i
+                                        class="fas fa-bars"></i></button>
+                            </div>
+                        </div>
+                        <div class="col-auto col-xxl-2 d-none d-xxl-block all-padding-0">
+                            <div class="home-header-one-location">
+                                	+91 9100069001 / 002     
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
